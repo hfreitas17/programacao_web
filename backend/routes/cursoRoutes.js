ec744ebfcc528388d958de4b1e80f0d4e5db2d47
Hamilton Freitas <hamilton.freitas@hotmail.com.br>
2025-07-21 12:58:13 -0300

    Versão 1.2

--- backend/routes/cursoRoutes.js
+++ backend/routes/cursoRoutes.js
@@ -10,14 +10,3 @@ router.post('/cursos', cadastrarCurso);
 router.get('/cursos', listarCursos);
 
 module.exports = router;
-
-
-/*const express = require('express');
-const { cadastrarCurso, listarCursos } = require('../controllers/cursoController');
-
-const router = express.Router();
-
-router.post('/cursos', cadastrarCurso);      // Rota para cadastrar curso
-router.get('/cursos', listarCursos);         // Rota para listar cursos
-
-module.exports = router;*/
